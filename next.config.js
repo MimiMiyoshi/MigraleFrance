2efc518d7c9cc27f71e685b1fc192caeff68fec5
MimiMiyoshi <amalfitana.campania@gmail.com>
2025-03-23 22:52:13 +0900

    Refactor: Simplify path alias configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "app"),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
